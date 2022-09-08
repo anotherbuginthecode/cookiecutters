0b3ab83cb910c9d888c61a88faf5bddc6a03fb3c
anotherbuginthecode <mangone.devops@gmail.com>
2022-09-08 12:52:34 +0200

    restore cookiecutter.json file

--- aws/lambda/python/hooks/pre_gen_project.sh
+++ aws/lambda/python/hooks/pre_gen_project.sh
@@ -8,9 +8,7 @@ error=$(tput setaf 160)
 warn=$(tput setaf 214)
 reset=$(tput sgr0)
 
-read -p "Do you need to create a lambda layer [y/N]? " CREATE_LAYER
-
-if [ $(echo "$CREATE_LAYER" | awk '{print tolower($0)}') == "y" ] ||  [ $(echo "$CREATE_LAYER" | awk '{print tolower($0)}') == "yes" ];
+if [ {{ cookiecutter.create_lambda_layer }} == "y" ];
 then
     echo ${info}INFO: ${reset}Creating layer folder${reset}
     mkdir ./layer
@@ -18,9 +16,7 @@ then
     echo "Remember to save all modules under the layer folder using ${bold}pip install --target=<path-to>/layer/ -r <path-to>/requirements.txt ${reset}before upload to AWS${reset}"
 fi
 
-read -p "Do you need to create a virtual enviroment [y/N]? " CREATE_ENV
-
-if [ $(echo "$CREATE_ENV" | awk '{print tolower($0)}') == "y" ] ||  [ $(echo "$CREATE_ENV" | awk '{print tolower($0)}') == "yes" ];
+if [ {{ cookiecutter.create_local_virtualenv }} == "y" ];
 then
     echo "${info}INFO: ${reset}Creating virtual environment...${reset}"
     python3 -m venv venv
