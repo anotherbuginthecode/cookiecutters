bdc079be62c85fa6f3c7bb3c0f1ecb1ea922f161
anotherbuginthecode <mangone.devops@gmail.com>
2022-09-08 12:15:16 +0200

    pre-hook to create a lambda layer if needed and virtual environment

--- aws/lambda/python/hooks/pre_gen_project.sh
+++ aws/lambda/python/hooks/pre_gen_project.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-echo "Hello world! {{ cookiecutter.repo_name }}"
+bold=$(tput bold)
+underline=$(tput smul)
+italic=$(tput sitm)
+info=$(tput setaf 2)
+error=$(tput setaf 160)
+warn=$(tput setaf 214)
+reset=$(tput sgr0)
+
+read -p "Do you need to create a lambda layer [y/N]?" CREATE_LAYER
+
+if [ echo "$CREATE_LAYER" | awk '{print tolower($0)}' == "y" ] ||  [ echo "$CREATE_LAYER" | awk '{print tolower($0)}' == "yes" ];
+then
+    mkdir ./layer
+    echo "${info}INFO:${reset}Layer folder created!${reset}"
+    echo "Remember to save all modules under the layer folder using ${bold}pip install --target=<path-to>/layer/ -r <path-to>/requirements.txt ${reset}before upload to AWS${reset}"
+fi
+
+echo "${info}INFO:${reset}Creating virtual environment...${reset}"
+python3 -m venv venv
+echo "${info}INFO:${reset}Virtual environment created!${reset}"
