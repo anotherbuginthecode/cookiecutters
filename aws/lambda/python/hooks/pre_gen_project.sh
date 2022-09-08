4b256c72437e5603cd9e0ac57ad192db9a2e1d9c
anotherbuginthecode <mangone.devops@gmail.com>
2022-09-08 12:28:10 +0200

    pre-hook to create a lambda layer if needed and virtual environment

--- aws/lambda/python/hooks/pre_gen_project.sh
+++ aws/lambda/python/hooks/pre_gen_project.sh
@@ -8,15 +8,21 @@ error=$(tput setaf 160)
 warn=$(tput setaf 214)
 reset=$(tput sgr0)
 
-read -p "Do you need to create a lambda layer [y/N]?" CREATE_LAYER
+read -p "Do you need to create a lambda layer [y/N]? " CREATE_LAYER
 
-if [ echo "$CREATE_LAYER" | awk '{print tolower($0)}' == "y" ] ||  [ echo "$CREATE_LAYER" | awk '{print tolower($0)}' == "yes" ];
+if [ $(echo "$CREATE_LAYER" | awk '{print tolower($0)}') == "y" ] ||  [ $(echo "$CREATE_LAYER" | awk '{print tolower($0)}') == "yes" ];
 then
+    echo ${info}INFO: ${reset}Creating layer folder${reset}
     mkdir ./layer
-    echo "${info}INFO:${reset}Layer folder created!${reset}"
+    echo "${info}INFO: ${reset}Layer folder created!${reset}"
     echo "Remember to save all modules under the layer folder using ${bold}pip install --target=<path-to>/layer/ -r <path-to>/requirements.txt ${reset}before upload to AWS${reset}"
 fi
 
-echo "${info}INFO:${reset}Creating virtual environment...${reset}"
-python3 -m venv venv
-echo "${info}INFO:${reset}Virtual environment created!${reset}"
+read -p "Do you need to create a virtual enviroment [y/N]? " CREATE_ENV
+
+if [ $(echo "$CREATE_ENV" | awk '{print tolower($0)}') == "y" ] ||  [ $(echo "$CREATE_ENV" | awk '{print tolower($0)}') == "yes" ];
+then
+    echo "${info}INFO: ${reset}Creating virtual environment...${reset}"
+    python3 -m venv venv
+    echo "${info}INFO: ${reset}Virtual environment created!${reset}"
+fi
