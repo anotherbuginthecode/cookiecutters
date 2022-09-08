8f569584662b4feae0869f3457c8188dcc9c98a3
anotherbuginthecode <mangone.devops@gmail.com>
2022-09-08 17:17:43 +0200

    add github  actions to template and git init

--- aws/lambda/hooks/post_gen_project.sh
+++ aws/lambda/hooks/post_gen_project.sh
@@ -9,7 +9,7 @@ extension="${filename##*.}"
 
 if [ "$extension" == "py" ];
 then
-echo " ${info}INFO: ${reset}Creating the file code/{{cookiecutter.handler_file}}${reset}"
+echo "${info}INFO: ${reset}Creating the file code/{{cookiecutter.handler_file}}${reset}"
 cat << EOF > code/{{cookiecutter.handler_file}}
 import json
 
@@ -21,36 +21,22 @@ def lambda_handler(event, context):
     }
 EOF
 
-echo " ${info}INFO: ${reset}Creating file code/requirements.txt${reset}"
+echo "${info}INFO: ${reset}Creating file code/requirements.txt${reset}"
 touch "code/requirements.txt"
 fi
 
+if [ {{ cookiecutter.cicd_github_action }} == "N" ];
+then
+    rm -rf .github/
+if
+
+if [ {{ cookiecutter.git_init }} == "y" ];
+then
+    git init
+    git add .
+    git commit -m "First commit"
+if
+
 echo ""
 echo ""
-echo "Enjoy your project and remember to ${bold}Praise the sun!${reset}"
-cat << EndOfMessage
-   &                                                                         ,  
-    *&                                                                    &&.   
-    .&&&&                                                              %&&&&&&  
-     &&((##                     (                                    %###.      
-        .####.,                   (                                ,#####       
-          ////*//                   ((######                    **//,//         
-            //*//*//                 ###(((##                 ////////          
-             */**/**,,, .            (  ##,./               ,//*/////           
-                . ,,/////// / .      ####(###(         ///////,,.//.            
-                  ,/,*/*///////.    /(##(####(.    ////.,///,///.               
-                    /,//*/*///////@./(((##((,##%  ////////**,//                 
-                        ///,.,////.@@@@@@@@@@@@@///*,//////                     
-                          ///,@@@@#@@@@(/@@@@&@@@@@@@/,//.                      
-                           ,/*@@@@&//@@//@@//@@@@@@@,//                         
-                            //#@@@@@/&&&&&#/@@@@@@@*/                           
-                              .(@#@(/&&*#&&///(/@@@/                         
-                               @@@@///(&&////@@@@@@                             
-                              @@@@/#@@@(/@@@&//@@@@@                            
-                              @@@@@@@@@@/(@@@@@@@@@@                            
-                               @@@@@@@@@@@@@@@@@@@@(                            
-                              .,....*.*(%@@@@@@@@@@                             
-                              .,,,,,,. ....,,,,,,,,                             
-                              @@@@@@%( ,,,,,.,@@@@@@                            
-                             /@@@@@@@@@@@@@@/.,,,,.@@                           
-EndOfMessage
+echo "Enjoy your project and remember to ${bold}Praise the sun! \[T]/ ${reset}"
